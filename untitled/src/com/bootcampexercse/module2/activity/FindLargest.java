3d4e9fb9f82b89e65b81c251f3aeee9a9806a77d
Alex77777 <alexeystr@yahoo.co.uk>
2022-07-25 17:13:56 +0300

    Create FindLargest.java

---
+++ untitled/src/com/bootcampexercse/module2/activity/FindLargest.java
@@ -0,0 +1,15 @@
+package com.bootcampexercse.module2.activity;
+
+public class FindLargest {
+    public static void main(String []args) {
+        int z = 25;
+        int y = 3;
+        if(z>y) {
+            System.out.println("z is greater then y");
+        }else if (z==y){
+        System.out.println ("y is equal to z");
+        }else {
+            System.out.println("y is greater then z");
+        }
+    }
+}
