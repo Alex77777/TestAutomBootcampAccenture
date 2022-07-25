4df7ff582878377b7a76c0516ec78263cb9ad4bc
Alex77777 <alexeystr@yahoo.co.uk>
2022-07-25 16:38:56 +0300

    Create OperatorActivity.java
    task 2.2

---
+++ untitled/src/com/bootcampexercse/module2/activity/OperatorActivity.java
@@ -0,0 +1,13 @@
+package com.bootcampexercse.module2.activity;
+
+public class OperatorActivity {
+    public static void main (String args []) {
+
+        int x = 1;
+        int y = 5;
+        int sum = x+y;
+        System.out.println(sum);
+
+    }
+
+}
