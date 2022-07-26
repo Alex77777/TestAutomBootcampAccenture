11a94e7a595d5dfd5015a3f85b9f1437ae468119
Alex77777 <alexeystr@yahoo.co.uk>
2022-07-26 09:58:20 +0300

    AddWholeNum
    AddWhole Num loop

---
+++ untitled/src/com/bootcampexercse/module2/activity/AddWholeNum.java
@@ -0,0 +1,16 @@
+package com.bootcampexercse.module2.activity;
+
+public class AddWholeNum {
+
+    public static void main(String []args) {
+        int sum = 0;
+        for (int i = 50; i <= 100; i++)
+        {
+            sum = sum + i;
+        }
+
+            System.out.println("sum of 50 numbers is" + sum);
+        }
+    }
+
+
